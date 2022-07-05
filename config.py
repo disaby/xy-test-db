1c360c190c2001619b82e6c63dd5358ead25795a
disaby <disaby@mail.ru>
2022-07-06 04:28:18 +0600

    +db

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5360890855:AAGwyKZmjsmAPmpboD1H3cjXcV9TIX5JqGA"
 APP_URL = "https://xy-test-db.herokuapp.com/" + BOT_TOKEN
+DB_URI = "mysql://b5aee84ce75dcf:ead1def2@eu-cdbr-west-03.cleardb.net/heroku_372d47352269b40?reconnect=true"
